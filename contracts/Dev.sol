f66c8f49d18147c323b3aedb80ccca2d4cf433eb
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-29 02:13:09 +0200

    add

--- contracts/Dev.sol
+++ contracts/Dev.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Dev is ERC20 {
-    constructor(uint initialSupply) ERC20("Dev Token", "DEV") {
-    _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupply) ERC20("Dev Token", "DEV") {
+        _mint(msg.sender, initialSupply);
     }
 }
--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -2,28 +2,41 @@
 
 pragma solidity ^0.8.4;
 
-import "./Dev.sol"
+import "./Dev.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract ICO {
-    uint private _balance;
-    uint private _offer;
-    uint private _price;
-    uint private _endTime;
+    uint256 private _balance;
+    uint256 private _offer;
+    uint256 private _price;
+    uint256 private _endTime;
     address private _owner;
-    constructor(uint offer_, uint price_, uint time) {
-        require(balanceOf(msg.sender) >= offer_, "ICO: balance of sender less than offer");
+
+    constructor(
+        uint256 offer_,
+        uint256 price_,
+        uint256 time
+    ) {
+        //require(balanceOf(msg.sender) >= offer_, "ICO: balance of sender less than offer");
         _offer = offer_;
         _price = price_;
-        _endTime = block.timestamp + time days;
+        _endTime = block.timestamp + time;
         _owner = msg.sender;
     }
-    function offer() public view returns (uint) {
+
+    function offer() public view returns (uint256) {
         return _offer;
     }
 
     function buy() public payable {
         require(msg.value / _price >= _offer, "ICO: offer less than amount sent");
         _balance += msg.value;
-        _transfer(_owner, msg.sender, msg.value / price);
+        //_transfer(_owner, msg.sender, msg.value / _price);
+    }
+
+    function withdraw() public {
+        require(msg.sender == _owner, "ICO: reserved to owner");
+        // payable(msg.sender).sendValue(address(this).balance);
     }
 }
