a50c61ebd1599edce33e9c9b4d12abf7d3cd2293
CryptoThibault <thibault.chalaoux@gmail.com>
2021-06-07 17:16:11 +0200

    docgen config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-solhint');
+require('hardhat-docgen');
 
 module.exports = {
   solidity: '0.8.4',
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
+  },
 };
